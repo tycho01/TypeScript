6b1af9bbe03ebfe80553844957253592a587c3c7
Tycho Grouwstra <tychogrouwstra@gmail.com>
2017-08-16 22:10:13 +0800

    add more tests

--- tests/cases/compiler/dontWiden.ts
+++ tests/cases/compiler/dontWiden.ts
@@ -7,12 +7,16 @@ interface SomeInterface { a: boolean }
 declare function foo<T extends any[]>(arg: T): { hi: T };
 declare function bar(arg: SomeInterface): void
 declare function baz(arg: [number, 2, 3 | number]): void
+declare function bag(arg: number[]): void
 
 // As variable assignees
 const a: number[] = [1, 2, 3];
 const b: SomeInterface = {a: true};
+const e = [1, 2, 3];
 
 // Same, but as arguments
 foo([1, 2, 3]);
 bar({a: true});
 baz([1, 2, 3]);
+bag([1, 2, 3]);
+bag(e);
