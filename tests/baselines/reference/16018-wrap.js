5b4375e589e540995a3d1799d8e85b901d1eac3b
Tycho Grouwstra <tychogrouwstra@gmail.com>
2017-08-26 00:25:58 +0800

    add wrap test, working

---
+++ tests/baselines/reference/16018-wrap.js
@@ -0,0 +1,15 @@
+//// [16018-wrap.ts]
+type Inc = { [k: string]: string; 0:'1',1:'2',2:'3',3:'4',4:'5'};
+type ObjectHasKey<O extends {}, K extends string> = ({[K in keyof O]: '1' } & { [k: string]: '0' })[K];
+type Length<R extends {}, I extends string = '0'> = { 1: Length<R, Inc[I]>, 0: I }[ObjectHasKey<R, I>];
+type TestLenA = Length<{ 0: 'a', 1: 'b' }>;
+// ^ ok, 2
+type Wrap<O> = Length<O>;
+// type Wrap<O, Len extends Length<O> = Length<O>> = Len;
+// ^ old work-around, same result here
+type TestLenB = Wrap<{ 0: 'a', 1: 'b' }>;
+// ^ 0 :(
+
+
+//// [16018-wrap.js]
+// ^ 0 :(
